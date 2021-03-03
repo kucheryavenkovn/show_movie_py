068d09d73ceaab1730f023602f76c54e6a96fbbf
kucheryavenkovn <kucheryavenkovn@gmail.com>
2021-03-03 15:10:12 +0700

    второй вариант

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import numpy as np
 import cv2
+import os
 
 cap = cv2.VideoCapture('video.mp4')
+cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+
+clicked = False
+def onMouse(event, x, y, flags, param):
+    global clicked
+    if event == cv2.EVENT_LBUTTONUP:
+        clicked = True
+
+cv2.setMouseCallback('window', onMouse)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('window', frame)
+    if cv2.waitKey(1) > -1 or clicked:
         break
 
 cap.release()
