db2e7d1b9481c0a140abfe0a07b933c759231ace
kucheryavenkovn <kucheryavenkovn@gmail.com>
2021-03-04 10:07:06 +0700

    окончательный вариант

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import numpy as np
 import cv2
-import os
 import win32gui
 import win32con
 import ctypes
 from ctypes import wintypes
 
 
-def bring_to_front(hwvn):
+def bring_to_front(hwnd):
     user32 = ctypes.WinDLL("user32")
     user32.SetWindowPos.restype = wintypes.HWND
     user32.SetWindowPos.argtypes = [wintypes.HWND, wintypes.HWND, wintypes.INT, wintypes.INT, wintypes.INT,
@@ -17,32 +15,23 @@ def bring_to_front(hwvn):
     user32.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, 0x0001)
     win32gui.SetActiveWindow(hwnd)
 
-def onMouse(event, x, y, flags, param):
-    global clicked
-    if event == cv2.EVENT_LBUTTONUP:
-        clicked = True
 
-
-cap = cv2.VideoCapture('E:\\kvn\\git\\show_movie_py\\video.mp4 ')
-cv2.namedWindow("window8march", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("window8march",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_OPENGL)
-
-hwnd = win32gui.FindWindow(None, 'window8march')
-
-clicked = False
-
-cv2.setMouseCallback('window8march', onMouse)
+wndname = "window8march"
+filename = '\\\\srv1-dc\\NETLOGON\\video.mp4'
+cap = cv2.VideoCapture(filename)
+cv2.namedWindow(wndname, cv2.WND_PROP_FULLSCREEN)
+HWND = win32gui.FindWindow(None, wndname)
 success, frame = cap.read()
-cv2.setWindowProperty("window8march", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.setWindowProperty(wndname, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-while (cv2.waitKey(1) != 27):
+while cv2.waitKey(1) != 27:
 
     if frame is not None:
-        cv2.imshow('window8march', frame)
+        cv2.imshow(wndname, frame)
     else:
-        cap = cv2.VideoCapture('video.mp4')
+        cap = cv2.VideoCapture(filename)
     success, frame = cap.read()
-    bring_to_front(hwnd)
+    bring_to_front(HWND)
 
 cap.release()
 cv2.destroyAllWindows()
